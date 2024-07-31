a571d03dc094b5885899306f19a811da26c0bfef
BijjamVijayReddy <bijjamvijayreddy@gmail.com>
2024-07-31 18:30:16 +0530

    modules import and export

---
+++ importExport/index.js
@@ -0,0 +1,9 @@
+const { add, name, dupliactes } = require("./modules");
+
+const numbers = [9, 4, 9, 1, 4, 6, 7, 2, 1, 6]
+
+console.log(add(5, 5));
+console.log(name);
+console.log(dupliactes(numbers))
+
+
---
+++ importExport/modules.js
@@ -0,0 +1,14 @@
+
+const add = (a, b) => {
+    return a + b
+};
+
+const dupliactes = (num) => {
+    const dupliactes = num.map((item) => item * 2);
+    return dupliactes
+}
+
+const name = "Vijay"
+
+
+module.exports = { add, name, dupliactes }
