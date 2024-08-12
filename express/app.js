9db9eb9da108153248a10f10962af4024dc50972
BijjamVijayReddy <bijjamvijayreddy@gmail.com>
2024-08-12 13:38:30 +0530

    changes are pushed

--- express/app.js
+++ express/app.js
@@ -1,17 +1,23 @@
+
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const apiData = require("../assests/product.json");
+const port = 8000;
+
+// Use the CORS middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Welcome  to Home page")
+    res.send("Welcome to HomePage")
 });
 
-// about url
+// userInfo;
 
-app.get("/about", (req, res) => {
-    res.send("Welcome to About Page")
+app.get("/users", (req,res) => {
+    res.send(apiData)
 })
 
-
-app.listen(8000, () => {
-    console.log("Server is Running Fine")
+app.listen(port, () => {
+    console.log(`Server is Running Sucessfully at port number ${port}`)
 })
